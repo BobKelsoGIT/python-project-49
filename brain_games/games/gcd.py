e2cb81d9d232b932562543e65ea8346de746394a
Ivan Scherbakov <lolwut155@gmail.com>
2023-10-23 00:08:45 +0300

    Fix GCD

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -2,20 +2,13 @@ import random
 from brain_games.game_logic import play_game
 
 
-def prime_factors(number):
+def prime_factors(num1, num2):
     """
     Find prime factorials for the number
     """
-    i = 2
-    factors = []
-    while i * i <= number:
-        while number % i == 0:
-            factors.append(i)
-            number = number / i
-        i = i + 1
-    if number > 1:
-        factors.append(number)
-    return factors
+    while num2:
+        num1, num2 = num2, num1 % num2
+    return num1
 
 
 def gcd_game():
@@ -27,16 +20,10 @@ def gcd_game():
     answers = []
 
     for _ in range(3):
-        first_number = random.randint(1, 100)
-        second_number = random.randint(1, 100)
-        numbers_for_calculations.append([first_number, second_number])
-        questions.append(([str(first_number) + ' ' + str(second_number)]))
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        questions.append(([str(num1) + ' ' + str(num2)]))
+        answers.append(str(prime_factors(num1, num2)))
 
-    for q in numbers_for_calculations:
-        list_of_prime_factors = [str(round(value)) for value in prime_factors(q[0]) if value in prime_factors(q[1])]
-        if list_of_prime_factors:
-            answers.append(str(max(list_of_prime_factors)))
-        else:
-            answers.append(str(1))
 
     play_game(questions, answers, description)
